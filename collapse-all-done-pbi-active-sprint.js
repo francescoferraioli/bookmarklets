1837ab283c9632a2a11025b0464da9a705aecff5
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2023-01-11 16:58:54 +1000

    Two jira bookmarklets

---
+++ collapse-all-done-pbi-active-sprint.js
@@ -0,0 +1,2 @@
+$('.ghx-swimlane-header [aria-expanded="false"] .aui-iconfont-expanded').click();
+Array.from($('.ghx-swimlane-header')).filter(x => $(".ghx-info > .jira-issue-status-lozenge-done", x).length === 1).forEach(x => $(".aui-icon", x).click());
