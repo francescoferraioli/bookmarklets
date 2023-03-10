efdab8fcbee50dc81f4a934ec46ada21886a639f
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2023-03-10 17:58:09 +1000

    Return if it selected it or it was already selected

--- jira/select-sprint.js
+++ jira/select-sprint.js
@@ -2,11 +2,12 @@ function selectSprint(sprint) {
   const dropdown = $('button[aria-controls="sprintPickerList"]');
 
   if(dropdown.text().includes(sprint))
-    return;
+    return false;
 
   const sprints = Array.from($("div > ul > li", dropdown.parent()));
 
   sprints.find(x => $(x).text().includes(sprint)).click();
 
   $(document).click();
+  return true;
 }
