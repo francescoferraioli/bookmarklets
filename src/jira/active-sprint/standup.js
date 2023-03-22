3a44ec7d95556ab72c15155a74922052d653ce3c
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2023-03-22 22:55:17 +1000

    Use partial for expand-all-closed in standup

--- src/jira/active-sprint/standup.js
+++ src/jira/active-sprint/standup.js
@@ -1,4 +1,4 @@
-$('.ghx-swimlane.ghx-closed .aui-icon').click();
+{{> jira__active-sprint__expand-all-closed-pbi }}
 
 function closeAllOfStatus(status) {
   {{> jira__active-sprint__click-all-pbi-of-status status="status" }}
