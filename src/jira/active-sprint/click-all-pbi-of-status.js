6e567cffbba2b14e8e05c1688cb5d2fb74cef596
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2023-03-22 22:15:01 +1000

    Take the status as a hbs parameter

--- src/jira/active-sprint/click-all-pbi-of-status.js
+++ src/jira/active-sprint/click-all-pbi-of-status.js
@@ -1,5 +1,5 @@
 Array.from(document.querySelectorAll(".jira-issue-status-lozenge"))
-  .filter(x => x.textContent === "$STATUS")
+  .filter(x => x.textContent === {{ status }})
   .map(x => x.closest(".ghx-swimlane-header")
   .querySelector(".ghx-expander"))
   .forEach(x => x.click())
