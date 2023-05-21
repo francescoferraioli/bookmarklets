bb29d9e79ffaad777f11f7c411e51cffea46ba1e
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2023-05-21 16:09:07 +1000

    Fix eslint issues

--- ts/hi.ts
+++ ts/hi.ts
@@ -1,7 +1,11 @@
 import { byeWorld, helloWorld } from "./util";
 
 export async function hi() {
+  // eslint-disable-next-line no-alert
   alert(helloWorld);
-  await new Promise((r) => setTimeout(r, 1000));
+  await new Promise((r) => {
+    setTimeout(r, 1000);
+  });
+  // eslint-disable-next-line no-alert
   alert(byeWorld);
 }
